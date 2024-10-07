3a58a39a03b6110b449ccac50bfe1db01dbb15cd
yuta <jp.yuta0131@gmail.com>
2024-10-08 02:40:22 +0900

    add: version関数を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+use pyo3::prelude::*;
 
+#[pymodule]
+fn my_python_lib(py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
+    // module.add_submodule(&fs_submodule(py)?)?;
+
+    module.add_function(wrap_pyfunction!(version, module)?)?;
+
+    Ok(())
+}
+
+#[pyfunction]
+fn version() -> PyResult<String> {
+    Ok(env!("CARGO_PKG_VERSION").to_string())
+}
