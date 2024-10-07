b7d28fe6150cda78e6b870be3e48392faf46ba1b
yuta <jp.yuta0131@gmail.com>
2024-10-08 02:58:33 +0900

    add: gen stub lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use pyo3::prelude::*;
+use pyo3_stub_gen::derive::gen_stub_pyfunction;
 
 #[pymodule]
-fn my_python_lib(py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
+fn my_python_lib(_: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
     // module.add_submodule(&fs_submodule(py)?)?;
 
     module.add_function(wrap_pyfunction!(version, module)?)?;
@@ -9,6 +10,7 @@ fn my_python_lib(py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
+#[gen_stub_pyfunction]
 #[pyfunction]
 fn version() -> PyResult<String> {
     Ok(env!("CARGO_PKG_VERSION").to_string())
